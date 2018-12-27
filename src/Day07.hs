bec620fadb36fcfc644f0ffbb0938525584dfca9
Dan Fuchs <dfuchs@jellyvision.com>
2018-12-27 14:51:45 -0600

    Day 7 part two works!  But it's soooooooo crappy...

--- src/Day07.hs
+++ src/Day07.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE ViewPatterns #-}
 module Day07 where
 
 import           AdventUtil                     ( inputFilePath )
@@ -14,21 +15,23 @@ type Edge = (Node, Node)
 type Dependencies = M.Map Node (S.Set Node)
 
 data Graph = Graph
-  { graphChildren :: M.Map Node (S.Set Node)
-  , graphParents :: M.Map Node (S.Set Node)
+  { children :: M.Map Node (S.Set Node)
+  , parents :: M.Map Node (S.Set Node)
+  , nodes :: S.Set Node
   }
   deriving (Show)
 
 fromEdges :: [Edge] -> Graph
-fromEdges = foldr absorb (Graph M.empty M.empty)
+fromEdges = foldr absorb (Graph M.empty M.empty S.empty)
  where
-  absorb (src, dst) (Graph children parents) = Graph
-    { graphChildren = M.insertWith S.union src (S.singleton dst) children
-    , graphParents  = M.insertWith
+  absorb (src, dst) Graph { children, parents, nodes } = Graph
+    { children = M.insertWith S.union src (S.singleton dst) children
+    , parents  = M.insertWith
       S.union
       src
       S.empty
       (M.insertWith S.union dst (S.singleton src) parents)
+    , nodes    = (S.insert dst (S.insert src nodes))
     }
 
 getReady :: Dependencies -> S.Set Node -> [Node]
@@ -37,10 +40,11 @@ getReady parents processed =
   in  M.keys $ M.filter (`S.isSubsetOf` processed) notProcessed
 
 process :: Graph -> [Node]
-process graph@(Graph children parents) = findPath (getReady parents S.empty)
-                                                  S.empty
-                                                  []
-                                                  (Graph children parents)
+process graph@(Graph children parents nodes) = findPath
+  (getReady parents S.empty)
+  S.empty
+  []
+  (Graph children parents)
  where
   findPath [] _ path _ = path
   findPath ready processed path graph =
@@ -63,59 +67,69 @@ data Worker = Worker
 data Pool = Pool
   { free :: [Worker]
   , busy :: [Worker]
+  , inProgress :: S.Set Node
   } deriving (Show)
 
 mkPool :: Int -> Pool
-mkPool numWorkers =
-  Pool {free = (take numWorkers $ repeat (Worker '-' 0)), busy = []}
+mkPool numWorkers = Pool
+  { free       = (take numWorkers $ repeat (Worker '-' 0))
+  , busy       = []
+  , inProgress = S.empty
+  }
 
 takeJob :: Node -> Worker
-takeJob node = Worker node (duration (trace ("Node: " ++ show node ++ "Duration: " ++ show (duration node)) node))
-
-inProgress :: Pool -> S.Set Node
-inProgress Pool { free, busy } = S.fromList $ node <$> busy
+takeJob node = Worker node (duration node)
 
 work :: Pool -> (Pool, [Node])
-work Pool { free, busy } =
+work Pool { free, busy, inProgress } =
   let worked            = doWork <$> busy
       (done, stillBusy) = L.partition isDone worked
-  in  ((Pool (free ++ done) stillBusy), (node <$> done))
+      newFree           = free ++ done
+      processed         = node <$> done
+      newInProgress     = S.difference inProgress (S.fromList processed)
+  in  ((Pool newFree stillBusy newInProgress), processed)
  where
   doWork Worker { node, timeRemaining } = Worker node (timeRemaining - 1)
   isDone Worker { timeRemaining } = timeRemaining == 0
 
-available :: Graph -> S.Set Node -> Pool -> [Node]
-available (Graph children parents) processed pool =
-  let allReady = getReady parents processed
-      working  = inProgress pool
-  in  S.toAscList ((S.fromList allReady) S.\\ working)
-
 takeJobs :: Pool -> [Node] -> Pool
-takeJobs pool@Pool { free = [], busy } ready = pool
-takeJobs pool                          []    = pool
-takeJobs Pool { free = (w : ws), busy } (r : rs) =
-  Pool ws (busy ++ [takeJob r])
+takeJobs pool@Pool { free = [], busy } ready    = pool
+takeJobs pool                          []       = pool
+takeJobs Pool { free = (w : ws), busy, inProgress } (n : ns) = takeJobs
+  Pool
+    { free       = ws
+    , busy       = (busy ++ [takeJob n])
+    , inProgress = (S.insert n inProgress)
+    }
+  ns
+
+count :: Pool -> Int
+count Pool { free, busy } = length free + length busy
 
 processWithWorkers :: Graph -> Int -> (Int, [Node])
-processWithWorkers graph@(Graph children parents) numWorkers =
-  let initialPool      = mkPool numWorkers
-      initialReady     = (getReady parents S.empty)
-      initialInProgress = []
-      initialProcessed = S.empty
-      initialTime      = 0
-      initialPath      = []
-  in  tick initialPool initialReady initialInProgress initialProcessed initialTime initialPath
+processWithWorkers graph@Graph { children, parents, nodes = allNodes } numWorkers
+  = let time        = 0
+        path        = []
+        processed   = S.empty
+        ready       = getReady parents processed
+        initialPool = mkPool numWorkers
+        pool        = takeJobs initialPool ready
+        remaining   = allNodes
+    in  tick time path pool processed remaining
  where
-  tick _ [] [] _ time path = (time, path)
-  tick pool ready progress processed time path =
-    let (workedPool, doneNodes) = work pool
-        newProcessed            = S.union processed (S.fromList doneNodes)
-        newPool                 = takeJobs workedPool ready
-        newPath                 = path ++ doneNodes
-        newReady                = available graph newProcessed newPool
-        newTime                 = time + 1
-        newProgress = S.toList $ inProgress newPool
-    in  tick newPool newReady newProgress newProcessed newTime newPath
+  tick time path _ _ (S.null -> True) = (time, path)
+  tick time path pool processed remaining =
+    let newTime                     = (traceShow time time) + 1
+        (workedPool, finishedNodes) = work pool
+        newPath                     = path ++ (L.sort finishedNodes)
+        newProcessed = S.union processed (S.fromList finishedNodes)
+        newRemaining                = S.difference remaining newProcessed
+        newAvailable                = S.toAscList $ S.difference
+          (S.fromList $ getReady parents newProcessed)
+          (inProgress workedPool)
+        newPool = takeJobs workedPool newAvailable
+    in  tick newTime newPath newPool newProcessed newRemaining
+
 
 parseEdge :: String -> Edge
 parseEdge input = (input !! 5, input !! 36)
@@ -138,6 +152,10 @@ day7b = do
   let rawEdges = lines content
   let edges    = parseEdge <$> rawEdges
   let graph    = fromEdges edges
-  let blah     = processWithWorkers graph 1
+  -- let pool     = mkPool 5
+  -- let newPool  = takeJobs pool "ABCD"
+  -- print newPool
+  let blah     = processWithWorkers graph 5
   print blah
+  print graph
 
